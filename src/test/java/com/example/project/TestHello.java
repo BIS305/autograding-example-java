57f2989e62163c1a61fc8090381d00dc14e65a66
m2web <m2web@yahoo.com>
2020-12-30 18:34:07 -0500

    Update java and test code

--- src/test/java/com/example/project/TestHello.java
+++ src/test/java/com/example/project/TestHello.java
@@ -2,11 +2,7 @@ package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
 import java.io.*;
 
 public class TestHello {
@@ -22,7 +18,7 @@ public class TestHello {
      Hello.main(null);
 
      // assertion
-     assertEquals("Hello world!\n", bos.toString());
+     assertEquals("Hello World!", bos.toString());
 
      // undo the binding in System
      System.setOut(originalOut);
